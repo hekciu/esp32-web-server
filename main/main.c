fbcf0b62dbba74ea68971d6f47bf0d825537faf4
hekciu <jakublasecki321@gmail.com>
2025-03-16 21:13:16 +0100

    init

---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+esp_netif_t * wifi_init_softap() {
+    esp_netif_t * esp_netif_ap = esp_netif_create_default_wifi_ap();
+}
+
+
+
+void app_main(void) {
+    printf("DUPAAAAAAA\n");
+}
